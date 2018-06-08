12ba97bd0a01d7e5e57eeaae16b20e632f97425e
smartserega <smartserega85@gmail.com>
2018-06-08 15:47:35 +0300

    new classes added (Rectangle, Square)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,26 +6,21 @@ public class MyFirstProgram {
         hello("User");
         hello("Sergey");
 
-        double l = 5;
+        Square s = new Square(5);
 
-        System.out.println("Площадь квадрата со стороной "+l + " равна " + area(l));
+        System.out.println("Площадь квадрата со стороной "+s.l + " равна " + s.area());
 
-        double a = 4;
-        double b = 6;
+        Rectangle r = new Rectangle(4,6);
 
-        System.out.println("Площадь прямоугольника со сторонами "+ a + " и " + b + " равна " + area(a,b));
+        System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b + " равна " + r.area() );
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (double len)  {
-        return len*len;
-    }
 
-    public static double area (double a, double b)  {
-        return a*b;
-    }
+
+
 
 }
