4bc6eef4aeee389be74b1bbc968ae5c07079a6bc
smartserega <smartserega85@gmail.com>
2018-06-26 14:32:33 +0300

    preconditions optimised

--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/appManager/GroupHelper.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/appManager/GroupHelper.java
@@ -59,4 +59,15 @@ public class GroupHelper extends HelperBase {
     public void returntoContactPage() {
         click(By.linkText("home"));
     }
+
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
 }
