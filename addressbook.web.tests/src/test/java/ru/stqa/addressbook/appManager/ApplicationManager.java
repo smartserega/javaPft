945409af7eb4fdfdff66415ed66ae03dc9d37960
smartserega <smartserega85@gmail.com>
2018-06-29 09:55:56 +0300

    group check tests added

--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/appManager/ApplicationManager.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/appManager/ApplicationManager.java
@@ -40,7 +40,7 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/group.php");
+        wd.get("http://localhost/addressbook");
         sessionHelper = new SessionHelper(wd);
         sessionHelper.login("admin", "secret");
         groupHelper = new GroupHelper(wd);
