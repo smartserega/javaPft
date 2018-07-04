04b99cbdf79b33a5a3e24425de986c11cae5d9e1
smartserega <smartserega85@gmail.com>
2018-07-04 10:18:21 +0300

    4.10 groups

--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
@@ -7,42 +7,21 @@ public class GroupData {
     private final String header;
     private final String footer;
 
+
     public void setId(int id) {
         this.id = id;
     }
 
     private int id;
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                '}';
-    }
-
     public int getId() {
         return id;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name) &&
-                Objects.equals(header, groupData.header);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(name, header);
-    }
 
     public GroupData(String name, String header, String footer) {
         this.name = name;
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.header = header;
         this.footer = footer;
     }
@@ -64,6 +43,28 @@ public class GroupData {
 
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
     }
 
 }
--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -26,9 +27,12 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(before.size() - 1);
+        before.remove(before.size() -1);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+        Comparator<? super GroupData> ById = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(ById);
+        after.sort(ById);
+        Assert.assertEquals(before, after);
     }
 
 }
