371f8a431c73c0e63647b46a7c432623d91a9c1c
smartserega <smartserega85@gmail.com>
2018-07-04 14:51:05 +0300

    contacts 4.9 final

--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/ContactsCreationTests.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/ContactsCreationTests.java
@@ -3,7 +3,6 @@ package ru.stqa.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.ContactsData;
-import ru.stqa.addressbook.model.GroupData;
 
 import java.util.HashSet;
 import java.util.List;
@@ -14,19 +13,14 @@ public class ContactsCreationTests extends TestBase {
     public void testContactsCreationTests() {
         app.getNavigationHelper().gotoContactPage();
         List<ContactsData> before = app.getContactsHelper().getContactList();
-        ContactsData contacts = new ContactsData("Test First name", "Test Middle name", "Test Last Name", "Test Nickname", "Test Title", "Test Compane", "Test Address", "+749511111111", "+790511111111", "E-mail@E-mail.ru", "Test1");
+        ContactsData contacts = new ContactsData("Test First name-1", "Test Middle name", "Test Last Name", "Test Nickname", "Test Title", "Test Compane", "Test Address", "+749511111111", "+790511111111", "E-mail@E-mail.ru", "Test1");
         app.getContactsHelper().createContact(contacts);
         app.getNavigationHelper().gotoContactPage();
         List<ContactsData> after = app.getContactsHelper().getContactList();
-        Assert.assertEquals(after.size(), before.size() +1);
+        Assert.assertEquals(after.size(), before.size() + 1);
 
 
-        int max = 0;
-        for (ContactsData g : after){
-            if(g.getId() > max){
-                max = g.getId();
-            }
-        }
+        int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
         contacts.setId(max);
         before.add(contacts);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
