dbc4ea31343cc99f498af1ceedd473e8bebafbe2
Inna Safonova <smartserega85@gmail.com>
2018-07-13 21:33:31 +0300

    6.3 final

--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/Generators/ContactDataGenerator.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/Generators/ContactDataGenerator.java
@@ -1,6 +1,10 @@
 package ru.stqa.addressbook.Generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.addressbook.model.ContactsData;
+import ru.stqa.addressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -10,15 +14,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
-        List<ContactsData> groups = generateContacts(count);
-        save(groups, file);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        }catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+
+        generator.run();
     }
 
-    private static void save(List<ContactsData> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactsData> contacts = generateContacts(count);
+        save(contacts, new File(file));
+    }
+
+    private void save(List<ContactsData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (ContactsData contact : contacts) {
@@ -27,7 +49,7 @@ public class ContactDataGenerator {
         writer.close();
     }
 
-    private static List<ContactsData> generateContacts(int count) {
+    private  List<ContactsData> generateContacts(int count) {
         List<ContactsData> contact = new ArrayList<ContactsData>();
         for (int i =0; i < count; i++) {
             contact.add(new ContactsData().withFirstName(String.format("firstName %s",i)).withLastName(String.format("lastName %s",i)).withMiddleName(String.format("middleName %s",i)));
--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/Generators/GroupDataGenerator.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/Generators/GroupDataGenerator.java
@@ -1,5 +1,9 @@
 package ru.stqa.addressbook.Generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import org.testng.annotations.Parameters;
 import ru.stqa.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,15 +14,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
-        List<GroupData> groups = generateGroups(count);
-        save(groups, file);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+
+    public static void main(String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        }catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+
+        generator.run();
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
+    }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
@@ -27,7 +49,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i =0; i < count; i++) {
             groups.add(new GroupData().withName(String.format("test %s",i)).withHeader(String.format("header %s",i)).withFooter(String.format("footer %s",i)));
