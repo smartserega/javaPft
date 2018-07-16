0d05faba616a8742e913587fec511946f5b67a75
smartserega <smartserega85@gmail.com>
2018-07-16 09:52:40 +0300

    6.4 groups

--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -1,24 +1,41 @@
 package ru.stqa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 import ru.stqa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreationTests() {
+
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("Test 1").withHeader("Header 1").withFooter("Footer 1")});
+        list.add(new Object[]{new GroupData().withName("Test 2").withHeader("Header 2").withFooter("Footer 2")});
+        list.add(new Object[]{new GroupData().withName("Test 3").withHeader("Header 3").withFooter("Footer 3")});
+        return list.iterator();
+
+
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreationTests(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("Test2");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
         assertThat(after, equalTo(
-                before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
     }
 
     @Test
