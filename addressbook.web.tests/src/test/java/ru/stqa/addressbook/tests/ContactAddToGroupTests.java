7eed13725fb3ad21cfa5af072cd1a6d9e4f01700
smartserega <smartserega85@gmail.com>
2018-07-30 15:26:43 +0300

    16 hw

--- addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/ContactAddToGroupTests.java
+++ addressbook.web.tests/src/test/java/ru/stqa/addressbook/tests/ContactAddToGroupTests.java
@@ -7,50 +7,96 @@ import ru.stqa.addressbook.model.ContactsData;
 import ru.stqa.addressbook.model.GroupData;
 import ru.stqa.addressbook.model.Groups;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 public class ContactAddToGroupTests extends TestBase {
 
-//    @BeforeMethod
-//    public void ensurePrecondtions() {
-//        app.db().groups();
-//
-//        if (app.db().groups().size() == 0) {
-//            app.goTo().groupPage();
-//            app.group().create(new GroupData().withName("TestAddContactGroup"));
-//
-//        } else if (!app.contacts().findGroupForAdd()) {
-//            app.goTo().groupPage();
-//            app.group().create(new GroupData().withName("TestAddContactGroup"));
-//        }
-//
-//        app.goTo().contactPage();
-//        Groups groups = app.db().groups();
-//        if (app.db().contacts().size() == 0) {
-//            app.contacts().create(new ContactsData().withFirstName("FirstName").withMiddleName("MiddleName").
-//                    withLastName("LastName").withNickname("nickname").withTitle("Title").withCompane("company").
-//                    withAddress("address").withMobile("111").withHomePhone("222").withEmail("E-mail@E-mail.ru").withWorkPhone("333").
-//                    inGroup(groups.iterator().next()));
-//        }
-//    }
+    @BeforeMethod
+    public void ensurePrecondtions() {
+        app.db().groups();
+
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("TestAddContactGroup"));
+        }
+
+        app.goTo().contactPage();
+        Groups groups = app.db().groups();
+        if (app.db().contacts().size() == 0) {
+            app.contacts().create(new ContactsData().withFirstName("FirstName").withMiddleName("MiddleName").
+                    withLastName("LastName").withNickname("nickname").withTitle("Title").withCompane("company").
+                    withAddress("address").withMobile("111").withHomePhone("222").withEmail("E-mail@E-mail.ru").withWorkPhone("333").
+                    inGroup(groups.iterator().next()));
+        }
+    }
 
     @Test
     public void addContactToGroupTests() {
-        ContactsData contact = new ContactsData();
-        int id = contact.getId();
-        findContactInGroup(id);
+        int groupSize = app.db().groups().size();
+        System.out.println("Размер груп равен " + groupSize);
+        int allContactSize = app.db().contacts().size();
+        System.out.println("Список всех контактов " + allContactSize);
+        System.out.println("увидим что " + findGoodContactForAdd(allContactSize));
     }
 
-    public ContactsData findContactInGroup(int id) {
+    private int findGoodContactForAdd(int allContactSize) {
         Contacts contacts = app.db().contacts();
         for (ContactsData contact : contacts) {
-            if (contact.getId() =)
-            System.out.println("!!!!!!!!"+contact.getGroups());
-            System.out.println(id+"!!!!!!!");
-        } return contact;
+            System.out.println("размер не знаю чего" + contact.getGroups().size());
+            if (contact.getGroups().size() == allContactSize) {
+                System.out.println("контакт не тот");
+            }
+            if (contact.getGroups().size() != allContactSize) {
+                System.out.println("контакт тот");
+                System.out.println("id нужного контакта " + contact.getId());
+                return contact.getId();
+            }
+
+        }
+        return allContactSize;
     }
 }
 
 
+//        Contacts contacts = app.db().contacts();
+//        int contactID = contacts.iterator().next().getId();
+//        findContactInGroup(contactID);
+//    }
+//
+//
+//    public ContactsData findContactInGroup(int contactID) {
+//        ContactsData contact = new ContactsData();
+//        contact = contact.withId(contactID);
+//        for (GroupData group : contact.getGroups())
+//                if (Objects.equals(group.getId(), contactID)) {
+//                    System.out.println("группа найдена");
+//                } else System.out.println("группа не найдена");
+//
+//        System.out.println(contact);
+//        return contact;
+//    }
+//}
 
 
+//
+//            System.out.println("groupsOfCurrentContact" + groupsOfCurrentContact);
+//            GroupData oneOfGroupWithId = groupsOfCurrentContact.iterator().next();
+//            GroupData groupWithAmendedId = oneOfGroupWithId.withId(id);
+////            System.out.println("!!!" + groupWithAmendedId);
+//        }
+
+
+//    давайте я ещё раз повторю ту схему, которую я предлагаю
+//      +  1) сначала узнаем общее количество групп в приложении
+//      +  2) потом получаем полный список контактов
+//        3) в цикле проходим по всем контактам, и для каждого из них проверяем размер списка групп,
+//          в которые этот контакт входит.
+//          - если размер совпадает с размером полного списка групп -- нам такой контакт не нужен.
+//          - если размер меньше -- значит этот контакт "хороший", подходит для наших целей, его можно в какую-то группу добавить
+//        описанное выше можно реализовать в виде метода с именем, скажем
+//        "найти контакт, который входит не во все группы"
+//        затем реализуем второй метод
+//        "найти группу, в которую не входит ЭТОТ контакт"
+//        внутри него
+//        1) получаем полный список групп
+//        2) получает список групп, в которые входит этот контакт
+//        3) в цикле проходим по полному списку и для каждой группы проверяем, входит она во второй список или нет
+//        как только находится группа, которая этому условию не удовлетворяет -- мы нашли подходящую группу
